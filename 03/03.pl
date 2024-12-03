73dfce8bc8e5ca0e994cb13eb1d09d18a00e8d09
Hannu Hartikainen <hannu@hrtk.in>
2024-12-03 21:25:07 +0200

    03.pl: more idiomatic

--- 03/03.pl
+++ 03/03.pl
@@ -1,10 +1,7 @@
 while(<>) { chomp; $in.=$_ }
-
-sub mulsum { my ($s)=@_, $sum=0;
+sub mulsum { my $sum=0;
     while($_[0] =~ /mul\((\d+),(\d+)\)/g) {
-        $sum += $1*$2 }
-    $sum }
+        $sum += $1*$2 } $sum }
 
 print mulsum($in)."\n";
-$in =~ s/don't\(\).*?(do\(\)|$)//g;
-print mulsum($in)
+print mulsum($in =~ s/don't\(\).*?(do\(\)|$)//gr)."\n";
