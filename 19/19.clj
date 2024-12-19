78616da6753d0a7078314c6bb62d34182728e848
Hannu Hartikainen <hannu@hrtk.in>
2024-12-19 14:22:20 +0200

    19.clj: star 2

--- 19/19.clj
+++ 19/19.clj
@@ -10,7 +10,19 @@
           (for [p (filter #(str/starts-with? towel %) patterns)]
             (possible patterns (str/replace-first towel p ""))))))
 
+(defn A' [a towel]
+  (if (empty? towel)
+    1
+    (apply +
+          (for [p (filter #(str/starts-with? towel %) patterns)]
+            (a a (str/replace-first towel p ""))))))
+(def A'' (memoize A'))
+(def A (partial A'' A''))
+
 (println (->> towels
              (map (partial possible patterns))
              (filter true?)
              count))
+(println (->> towels
+              (map A)
+              (apply +)))
